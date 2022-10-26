c48467f664c48d9cbb242c3f5d76a219f4b73ba9
Stephen Kilbourn <skilbourn@element84.com>
2022-10-25 21:22:30 -0500

    try dot reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['./src/index.ts']],
+  reporter: [['dot']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     actionTimeout: 0,
